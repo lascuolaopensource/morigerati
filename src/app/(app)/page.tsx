12779fdcc806dd33f8f0cecac734cf8b2637c43c
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-02 22:39:27 +0200

    added tailwind

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,7 @@
-export default function ciao() {
-  return <h3>Ciao Morigerati!</h3>
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+    </div>
+  )
 }
