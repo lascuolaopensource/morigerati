20ae0a7c01ca83c90caa94def730622d8edfb8be
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-04 15:45:34 +0200

    trying dynamic fetching

--- src/app/(app)/luoghi/page.tsx
+++ src/app/(app)/luoghi/page.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import { Suspense } from 'react'
+
 import { findCollection, findGlobals } from '@/utils/fetch'
 import Footer from '../../components/footer'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 const LuoghiPage = async () => {
   const collectionLuoghi = await findCollection({ collection: 'stakeholders' })
   const globalLuoghi = await findGlobals({ slug: 'testi' })
 
   return (
-    <div className="bg-white">
-      <h1>Luoghi</h1>
-      <pre>{JSON.stringify(globalLuoghi, null, 2)}</pre>
-      <h2>Componente galleria</h2>
-      <pre>{JSON.stringify(collectionLuoghi, null, 2)}</pre>
-      <Footer />
-    </div>
+    <main>
+      <div className="bg-white">
+        <h1>Luoghi</h1>
+        <pre>{JSON.stringify(globalLuoghi, null, 2)}</pre>
+        <h2>Componente galleria</h2>
+        <pre>{JSON.stringify(collectionLuoghi, null, 2)}</pre>
+        <Suspense fallback={<div>Loading footer...</div>}>
+          <Footer />
+        </Suspense>
+      </div>
+    </main>
   )
 }
 
