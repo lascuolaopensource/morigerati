bb8bf4097b77a91bc66e8576b4d7e98d222522b2
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-05 18:02:20 +0200

    menu

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import logoBlack from '@/public/logoBlack.png'
+import logo from '@/public/logo.png'
 import hamburger from '@/public/hamburger.png'
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className="w-full bg-gray-100 relative">
       <div className="py-4 px-6">
@@ -13,12 +21,63 @@ const Navbar = () => {
           <Link href="/" className="flex justify-center">
             <Image src={logoBlack} alt="Logo" width={100} />
           </Link>
-          <button className="w-8 h-8 flex items-center justify-center">
-            <Image src={hamburger} alt="Menu" width={20} />
+          <button
+            className="w-8 h-8 flex items-center justify-center z-50"
+            onClick={toggleMenu}
+            aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          >
+            {!isMenuOpen && <Image src={hamburger} alt="Menu" width={20} />}
           </button>
         </div>
       </div>
       <div className="absolute bottom-0 left-0 w-full h-0.5 bg-black"></div>
+
+      {/* Menu a scomparsa */}
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-black text-white z-40">
+          <div className="absolute top-0 left-0 right-0 py-4 px-6 flex justify-between items-center">
+            <div className="w-8"></div>
+            <div className="flex justify-center">
+              <Image src={logo} alt="Logo" width={100} />
+            </div>
+            <button
+              onClick={toggleMenu}
+              className="w-8 h-8 flex items-center justify-center text-3xl text-white"
+              aria-label="Close menu"
+            >
+              &times;
+            </button>
+          </div>
+          <ul className="flex flex-col items-center pt-24 space-y-6 text-2xl">
+            <li>
+              <Link href="/" className="flex items-center">
+                &rarr; Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/mobilita-sostenibile">Mobilità sostenibile</Link>
+            </li>
+            <li>
+              <Link href="/luoghi">Luoghi</Link>
+            </li>
+            <li>
+              <Link href="/itinerari">Itinerari</Link>
+            </li>
+            <li>
+              <Link href="/stakeholders">Stakeholders</Link>
+            </li>
+            <li>
+              <Link href="/residenze">Residenze</Link>
+            </li>
+            <li>
+              <Link href="/articoli">Articoli</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   )
 }
