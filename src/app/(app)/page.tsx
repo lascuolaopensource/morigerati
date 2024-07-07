a01e636b21c24bb08f5446f1bd7f3be8affea4b3
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-08 00:05:22 +0200

    multiple interface for the homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,47 +1,99 @@
-import Polaroid from '../components/polaroid'
-import Navbar from '../components/navbar'
-import { findCollection, findGlobals } from '@/utils/fetch'
+import React, { Suspense } from 'react'
+import { findGlobals } from '@/utils/fetch'
 
-interface Statement {
-  Home: {
-    statement: {}
+import Colorcard from '@/app/components/colorcard'
+import Navbar from '@/app/components/navbar'
+import Footer from '@/app/components/footer'
+
+import loremPic from '@/public/loremPic.png'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+interface GlobalTesti {
+  statement: string
+  testoHome: {
+    root: {
+      children: Array<{
+        children: Array<{
+          text: string
+        }>
+      }>
+    }
+  }
+  itinerari: {
+    root: {
+      children: Array<{
+        children: Array<{
+          text: string
+        }>
+      }>
+    }
+  }
+  luoghi: {
+    root: {
+      children: Array<{
+        children: Array<{
+          text: string
+        }>
+      }>
+    }
+    residenze: {
+      root: {
+        children: Array<{
+          children: Array<{
+            text: string
+          }>
+        }>
+      }
+    }
   }
 }
 
-interface TestoHome {
-  Home: {
-    testoHome: {}
-  }
-}
 const Home = async () => {
   let statement = null
+  let itinerariText = null
   let testoHome = null
-
   try {
-    const [globalData] = await Promise.all([findGlobals({ slug: 'home' })])
-
-    statement = JSON.stringify(globalData.statement)
-    testoHome = JSON.stringify(globalData.testoHome)
+    const globalData = (await findGlobals({ slug: 'home' })) as unknown as GlobalTesti
+    statement = globalData.statement
+    itinerariText = globalData.itinerari.root.children[0].children[0].text
+    testoHome = globalData.itinerari.root.children[0].children[0].text
   } catch (error) {
     console.error('Error fetching data:', error)
   }
 
   return (
-    <div className="bg-white">
-      <Navbar backgroundColor="bg-white" currentPage="/" />
-      {statement ? (
-        <p className="font-normal text-sm pt-4 pb-4 leading-4">{statement}</p>
-      ) : (
-        <p>Error loading Luoghi text data</p>
-      )}
-
-      {testoHome ? (
-        <p className="font-normal text-sm pt-4 pb-4 leading-4">{testoHome}</p>
-      ) : (
-        <p>Error loading Luoghi text data</p>
-      )}
-      {/*<Polaroid imageUrl="/loremPic.jpg" title="Ciao" color="bg-luogoColor" />*/}
-    </div>
+    <main>
+      <Navbar backgroundColor="bg-white" currentPage="/luoghi" />
+      <div className="relative">
+        <img src="/loremPic.png" alt="Fullscreen Image" className="w-full h-screen object-cover" />
+        <div className="absolute inset-0 flex items-end justify-center">
+          <div className="bg-white p-3 pt-5 w-full">
+            {testoHome ? (
+              <p className="font-normal text-sm pt-4 pb-4 leading-4">{testoHome}</p>
+            ) : (
+              <p>Error loading Luoghi text data</p>
+            )}
+            {itinerariText ? (
+              <p className="font-normal text-sm pt-4 pb-4 leading-4">{itinerariText}</p>
+            ) : (
+              <p>Error loading Luoghi text data</p>
+            )}
+            <Suspense fallback={<div>Loading Luogo component...</div>}>
+              <Colorcard color="bg-luogoColor" title="Luogo 1" />
+              <Colorcard color="bg-luogoColor" title="Luogo 2" />
+              <Colorcard color="bg-luogoColor" title="Luogo 3" />
+              <Colorcard color="bg-luogoColor" title="Luogo 4" />
+              <Colorcard color="bg-luogoColor" title="Luogo 5" />
+            </Suspense>
+          </div>
+        </div>
+      </div>
+      <Suspense fallback={<div>Loading footer...</div>}>
+        <Footer />
+      </Suspense>
+    </main>
   )
 }
 
