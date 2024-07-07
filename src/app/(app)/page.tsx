ba937ac0e46a1eb74d49d0552ea3420d76751b3b
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-07 18:52:09 +0200

    home, trying fetching with ts things, if this compile i'm a genius, if not typescript is the worst

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,11 +1,44 @@
 import Polaroid from '../components/polaroid'
 import Navbar from '../components/navbar'
+import { findCollection, findGlobals } from '@/utils/fetch'
 
-export default function Home() {
+interface Statement {
+  statement: {}
+}
+
+interface TestoHome {
+  testoHome: {}
+}
+const Home = async () => {
+  let statement = null
+  let testoHome = null
+
+  try {
+    const [globalData] = await Promise.all([findGlobals({ slug: 'home' })])
+
+    statement = JSON.stringify(globalData.statement)
+    testoHome = JSON.stringify(globalData.testoHome)
+  } catch (error) {
+    console.error('Error fetching data:', error)
+  }
+
   return (
     <div className="bg-white">
       <Navbar backgroundColor="bg-white" currentPage="/" />
+      {statement ? (
+        <p className="font-normal text-sm pt-4 pb-4 leading-4">{statement}</p>
+      ) : (
+        <p>Error loading Luoghi text data</p>
+      )}
+
+      {testoHome ? (
+        <p className="font-normal text-sm pt-4 pb-4 leading-4">{testoHome}</p>
+      ) : (
+        <p>Error loading Luoghi text data</p>
+      )}
       {/*<Polaroid imageUrl="/loremPic.jpg" title="Ciao" color="bg-luogoColor" />*/}
     </div>
   )
 }
+
+export default Home
