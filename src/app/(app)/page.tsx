48c6ee23fa8595162a6d4d7417789c6c04d4e63c
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-08 00:22:40 +0200

    splash image on home

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react'
 import { findGlobals } from '@/utils/fetch'
 
+import Image from 'next/image'
+
 import Colorcard from '@/app/components/colorcard'
 import Navbar from '@/app/components/navbar'
 import Footer from '@/app/components/footer'
@@ -66,30 +68,38 @@ const Home = async () => {
   return (
     <main>
       <Navbar backgroundColor="bg-white" currentPage="/luoghi" />
-      <div className="relative">
-        <img src="/loremPic.png" alt="Fullscreen Image" className="w-full h-screen object-cover" />
-        <div className="absolute inset-0 flex items-end justify-center">
-          <div className="bg-white p-3 pt-5 w-full">
-            {testoHome ? (
-              <p className="font-normal text-sm pt-4 pb-4 leading-4">{testoHome}</p>
-            ) : (
-              <p>Error loading Luoghi text data</p>
-            )}
-            {itinerariText ? (
-              <p className="font-normal text-sm pt-4 pb-4 leading-4">{itinerariText}</p>
-            ) : (
-              <p>Error loading Luoghi text data</p>
-            )}
-            <Suspense fallback={<div>Loading Luogo component...</div>}>
-              <Colorcard color="bg-luogoColor" title="Luogo 1" />
-              <Colorcard color="bg-luogoColor" title="Luogo 2" />
-              <Colorcard color="bg-luogoColor" title="Luogo 3" />
-              <Colorcard color="bg-luogoColor" title="Luogo 4" />
-              <Colorcard color="bg-luogoColor" title="Luogo 5" />
-            </Suspense>
-          </div>
-        </div>
+      <div className="relative w-full h-screen">
+        <Image src={loremPic} alt="Fullscreen Image" layout="fill" objectFit="cover" />
+
+        {statement ? (
+          <p className="absolute text-center font-bold top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-3xl">
+            {statement}
+          </p>
+        ) : (
+          <p>Error loading Luoghi text data</p>
+        )}
       </div>
+
+      <div className="bg-white p-3 pt-5 w-full">
+        {testoHome ? (
+          <p className="font-normal text-sm pt-4 pb-4 leading-4">{testoHome}</p>
+        ) : (
+          <p>Error loading Luoghi text data</p>
+        )}
+        {itinerariText ? (
+          <p className="font-normal text-sm pt-4 pb-4 leading-4">{itinerariText}</p>
+        ) : (
+          <p>Error loading Luoghi text data</p>
+        )}
+        <Suspense fallback={<div>Loading Luogo component...</div>}>
+          <Colorcard color="bg-luogoColor" title="Luogo 1" />
+          <Colorcard color="bg-luogoColor" title="Luogo 2" />
+          <Colorcard color="bg-luogoColor" title="Luogo 3" />
+          <Colorcard color="bg-luogoColor" title="Luogo 4" />
+          <Colorcard color="bg-luogoColor" title="Luogo 5" />
+        </Suspense>
+      </div>
+
       <Suspense fallback={<div>Loading footer...</div>}>
         <Footer />
       </Suspense>
