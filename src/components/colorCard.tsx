bb85fe8b84be577a296eb42c0615696e2b16eba6
Alessandro Miracapillo <alesim11@gmail.com>
2024-07-12 19:14:16 +0200

    pagina stakeholder

--- src/components/colorCard.tsx
+++ src/components/colorCard.tsx
@@ -10,7 +10,7 @@ interface ColorcardProps {
 
 const Colorcard: React.FC<ColorcardProps> = ({ color, title, imageUrl, slugUrl }) => {
   return (
-    <Link href={`/luoghi/${slugUrl}`} className="block w-full max-w-md mx-auto pb-1">
+    <Link href={`/stakeholders/${slugUrl}`} className="block w-full max-w-md mx-auto pb-1">
       <div className="border-2 border-black rounded overflow-hidden h-40 flex flex-col transition-transform duration-300 ease-in-out hover:scale-105">
         <div className={`${color} p-2 border-b-2 border-black`}>
           <h2 className="text-sm font-bold text-center leading-3">{title}</h2>
