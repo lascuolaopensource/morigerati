361e2575ddf039500a46cd2e3be39501b6f2aa4f
Alessandro Miracapillo <alesim11@gmail.com>
2024-09-14 20:05:33 +0200

    Add custom colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'luogoColor': '#92CDDE',
+        'itinerarioColor' : '#7FCBAE',
+        'stakeholderColor' : '#EEE3A8',
+        'residenzeColor' : '#F69679',
+        'bannerColor' : '#F69679'
+    },},
   },
   plugins: [],
 }
