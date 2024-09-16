dd7f3850ad71d7344f6c2bcd776ab9fa59793d11
Alessandro Miracapillo <alesim11@gmail.com>
2024-09-16 16:52:05 +0200

    Refactor ArticoliGrid component to display article titles, images, and index number

--- src/components/articoli/articoliGrid.tsx
+++ src/components/articoli/articoliGrid.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Articoli } from '@/payload-types'
+import Image, { StaticImageData } from 'next/image'
 
 interface ArticoliGridProps {
   articoli: Articoli[]
 }
 
-type TextContent = {
-  root: {
-    children: Array<{
-      children: Array<{
-        text: string
-      }>
-    }>
-  }
-}
-
 const ArticoliGrid: React.FC<ArticoliGridProps> = ({ articoli }): JSX.Element => {
   return (
     <>
-      {articoli.map((articolo) => (
-        <p key={articolo.id}>{articolo.titolo ?? 'No text available'}</p>
+      {articoli.map((articolo, n) => (
+        <div>
+          <p key={articolo.id}>{articolo.titolo ?? 'Senza Titolo'}</p>
+          <Image
+            src={
+              typeof articolo.media === 'string'
+                ? articolo.media
+                : articolo.media?.url || '/placeholder-image.jpg'
+            }
+            alt={articolo.titolo || 'Immagine articolo'}
+            width={40}
+            height={40}
+            className="object-cover"
+          />
+          <p>{n}</p>
+        </div>
       ))}
     </>
   )
