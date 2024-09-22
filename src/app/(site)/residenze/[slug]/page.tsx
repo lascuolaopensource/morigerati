2b0eaabdfc06b48f15357b6d69b95ef8183dcf42
Alessandro Miracapillo <alesim11@gmail.com>
2024-09-22 21:24:10 +0200

    momentaneo slug residenze

--- src/app/(site)/residenze/[slug]/page.tsx
+++ src/app/(site)/residenze/[slug]/page.tsx
@@ -1,1 +1,43 @@
-import next from 'next'
+import React, { Suspense } from 'react'
+import { loadDb } from '@/utils/db'
+import ColorCardWrapper from '@/components/colorCardWrapper'
+
+import StringToHTML from '@/components/serializer/stringToHTML'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+const Stakeholders = async () => {
+  const db = await loadDb()
+  const testi = await db.findGlobal({
+    slug: 'testi',
+  })
+
+  const stakeholders = await db.find({
+    collection: 'stakeholders',
+  }) //
+
+  return (
+    <main className="">
+      <div className="bg-white p-3 pt-5">
+        {testi.stakeholders.title ? (
+          <div className="font-normal text-sm pt-4 pb-10 leading-4">
+            <h1 className="font-bold text-[40px]">{testi.stakeholders.title}</h1>
+          </div>
+        ) : (
+          <p></p>
+        )}
+        <StringToHTML htmlString={testi.stakeholders.text_html ?? ''} />
+        <Suspense fallback={<div>Loading cards...</div>}>
+          <ColorCardWrapper
+            color="bg-stakeholderColor"
+            docs={stakeholders.docs}
+            previous="stakeholders"
+          />
+        </Suspense>
+      </div>
+    </main>
+  )
+}
+
+export default Stakeholders
