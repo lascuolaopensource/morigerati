234b5a67d79a1feafb44f87b8caa21bd3e008a2c
Alessandro Miracapillo <alesim11@gmail.com>
2024-10-30 12:27:08 +0100

    added icone

--- src/components/itinerarioDetailsCard.tsx
+++ src/components/itinerarioDetailsCard.tsx
@@ -1,5 +1,10 @@
 import { type Itinerari } from '@/payload-types'
 
+import { GiPathDistance } from 'react-icons/gi'
+import { LuTimer } from 'react-icons/lu'
+import { SiLevelsdotfyi } from 'react-icons/si'
+import { IoSpeedometerOutline } from 'react-icons/io5'
+
 type ItinerarioDetailsProps = Pick<
   Itinerari,
   'lunghezza' | 'tempo' | 'dislivello' | 'difficolta' | 'tipo'
@@ -8,12 +13,34 @@ type ItinerarioDetailsProps = Pick<
 interface DetailSectionProps {
   label: string
   value: string | number | null | undefined
+  icon?: string
+}
+interface IconsProps {
+  icon?: string
+}
+const Icons = ({ icon }: IconsProps) => {
+  if (icon == 'distance') {
+    return <GiPathDistance size={30} />
+  }
+  if (icon == 'time') {
+    return <LuTimer size={30} />
+  }
+  if (icon == 'dislivello') {
+    return <SiLevelsdotfyi size={30} />
+  }
+  if (icon == 'difficolta') {
+    return <IoSpeedometerOutline size={30} />
+  }
 }
 
-const DetailSection = ({ label, value }: DetailSectionProps) => (
-  <div className="h-24 border-2  border-itinerarioColorScuro bg-itinerarioColor p-2 text-black">
+const DetailSection = ({ label, value, icon }: DetailSectionProps) => (
+  <div className="h-24 border-2 rounded-xl border-itinerarioColorScuro bg-itinerarioColor p-2 text-black">
     <div className="flex h-full flex-col justify-between">
+      <div>
+        <Icons icon={icon} />
+      </div>
       <div className="h-6" />
+
       <span className=" text-sm">{label}</span>
       <span className=" text-sm font-bold ">{value ?? 'Non disponibile'}</span>
     </div>
@@ -27,7 +54,7 @@ const TipoSection = ({ tipo }: { tipo: ItinerarioDetailsProps['tipo'] }) => {
   }
 
   return (
-    <div className="col-span-2 border-2 border-itinerarioColorScuro bg-itinerarioColor p-2 text-black">
+    <div className="col-span-2 border-2 rounded-xl border-itinerarioColorScuro bg-itinerarioColor p-2 text-black">
       <div className="flex h-full justify-center">
         <span className="text-xl font-bold">{formatTipo(tipo)}</span>
       </div>
@@ -48,12 +75,12 @@ export default function ItinerarioDetailsCard({
   }
 
   return (
-    <div className="grid grid-cols-2 gap-px bg-white">
+    <div className="grid grid-cols-2 gap-2 bg-white">
       <TipoSection tipo={tipo} />
-      <DetailSection label="lunghezza" value={formatValue(lunghezza, 'km')} />
-      <DetailSection label="durata" value={formatValue(tempo, 'ore')} />
-      <DetailSection label="dislivello" value={formatValue(dislivello, 'mt')} />
-      <DetailSection label="difficoltà" value={formatValue(difficolta)} />
+      <DetailSection label="lunghezza" value={formatValue(lunghezza, 'km')} icon="distance" />
+      <DetailSection label="durata" value={formatValue(tempo, 'ore')} icon="time" />
+      <DetailSection label="dislivello" value={formatValue(dislivello, 'mt')} icon="dislivello" />
+      <DetailSection label="difficoltà" value={formatValue(difficolta)} icon="difficolta" />
     </div>
   )
 }
