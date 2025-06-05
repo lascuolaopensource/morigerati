81f539e8e82dfa4a67abe5abb383a84378444914
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-06-05 15:25:18 +0200

    fix: residenza state (#23)
    * fix: residenza state
    
    * fix: escape symbol

--- src/components/uiElements/button.tsx
+++ src/components/uiElements/button.tsx
@@ -17,7 +17,7 @@ export function Button(props: Props) {
   const { bg } = getColorTheme(color)
 
   const classes = cn(
-    'w-full rounded-full px-4 py-2 text-white text-nowrap text-center',
+    'rounded-full px-4 py-2 text-white text-nowrap text-center',
     'hover:cursor-pointer hover:scale-105 transition-transform duration-300',
     'flex items-center justify-center gap-1',
     bg,
