bd8276a95cc30a4d1e3a7a4b1d60a87a30eeef01
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-06-09 16:28:48 +0200

    style: format

--- src/db/fields/components/gap.tsx
+++ src/db/fields/components/gap.tsx
@@ -1,7 +1,7 @@
 import exp from 'constants'
 
 // import {} from '@payloadcms/ui/scss'
-const Gap = ({ size, key}: { size: number, key: string}) => {
+const Gap = ({ size, key }: { size: number; key: string }) => {
   //export function Gap(props: { size: number }) {
   return (
     <div
