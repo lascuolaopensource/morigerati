94007b7de1cff611964e2085554af417ccbc9033
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-07-04 11:00:10 +0200

    feat: setup auth page

--- src/app/(site)/[locale]/account/login/page.tsx
+++ src/app/(site)/[locale]/account/login/page.tsx
@@ -1,9 +1,43 @@
+'use client'
+
+import { signup } from './auth'
+import { useActionState } from 'react'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Label } from '@/components/ui/label'
+import { Alert, AlertTitle } from '@/components/ui/alert'
+import { AlertCircleIcon } from 'lucide-react'
+
+//
+
 export default function LoginPage() {
+  const [state, action, pending] = useActionState(signup, undefined)
+
   return (
-    <form>
-      <input className="border" type="email" />
-      <input className="border" type="password" />
-      <button type="submit">Login</button>
+    <form action={action} className="flex flex-col gap-6 max-w-md w-full mx-auto p-4">
+      <FieldWrapper>
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" name="email" type="email" />
+      </FieldWrapper>
+
+      <FieldWrapper>
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" name="password" type="password" />
+      </FieldWrapper>
+
+      {state?.error && (
+        <Alert variant="destructive">
+          <AlertCircleIcon />
+          <AlertTitle>{state.error}</AlertTitle>
+        </Alert>
+      )}
+
+      <Button type="submit">Login</Button>
     </form>
   )
+}
+
+function FieldWrapper({ children }: { children: React.ReactNode }) {
+  return <div className="w-full space-y-2">{children}</div>
 }
