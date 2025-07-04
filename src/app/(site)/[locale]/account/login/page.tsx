658789ce9e3c6e512491dacf3edb92dabb8082aa
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-07-04 09:59:52 +0200

    setup pages

---
+++ src/app/(site)/[locale]/account/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <form>
+      <input className="border" type="email" />
+      <input className="border" type="password" />
+      <button type="submit">Login</button>
+    </form>
+  )
+}
