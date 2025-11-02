da9c32b86de91f15a5c47dfe1e3f13322ba79ed9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-02 08:49:53 +0100

    complete i18n

---
+++ src/modules/i18n/localization.ts
@@ -0,0 +1,22 @@
+import type { LocalizationConfig } from 'payload'
+
+import { routing } from './routing'
+
+//
+
+const [it, en] = routing.locales
+
+export const localization: LocalizationConfig = {
+	defaultLocale: routing.defaultLocale,
+	fallback: true,
+	locales: [
+		{
+			code: it,
+			label: 'Italiano 🇮🇹',
+		},
+		{
+			code: en,
+			label: 'English 🇬🇧',
+		},
+	],
+}
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,57 +2,68 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import { s3Storage } from '@payloadcms/storage-s3'
+import { it } from '@payloadcms/translations/languages/it'
+import { localization } from '#/i18n'
 import path from 'path'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
 import { fileURLToPath } from 'url'
 
-import { Media } from './db/collections/Media'
-import { Users } from './db/collections/Users'
+import { Media } from '@/db/collections/Media'
+import { Users } from '@/db/collections/Users'
+
+//
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    importMap: {
-      baseDir: path.resolve(dirname),
-    },
-  },
-  collections: [Users, Media],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  db: postgresAdapter({
-    pool: {
-      connectionString: process.env.DATABASE_URI || '',
-    },
-  }),
-  sharp,
-  plugins: [s3()],
+	admin: {
+		user: Users.slug,
+		importMap: {
+			baseDir: path.resolve(dirname),
+		},
+	},
+	collections: [Users, Media],
+	editor: lexicalEditor(),
+	secret: process.env.PAYLOAD_SECRET || '',
+	typescript: {
+		outputFile: path.resolve(dirname, 'payload-types.ts'),
+	},
+	db: postgresAdapter({
+		pool: {
+			connectionString: process.env.DATABASE_URI || '',
+		},
+	}),
+	sharp,
+	plugins: [s3()],
+	localization: localization,
+	i18n: {
+		fallbackLanguage: 'it',
+		supportedLanguages: {
+			it: it,
+		},
+	},
 })
 
 //
 
 function s3() {
-  return s3Storage({
-    disableLocalStorage: true,
-    collections: {
-      [Media.slug]: true,
-    },
-    bucket: process.env.S3_BUCKET!,
-    enabled: true,
-    config: {
-      endpoint: process.env.S3_ENDPOINT!,
-      region: process.env.S3_REGION!,
-      credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
-      },
-      forcePathStyle: true,
-    },
-  })
+	return s3Storage({
+		disableLocalStorage: true,
+		collections: {
+			[Media.slug]: true,
+		},
+		bucket: process.env.S3_BUCKET!,
+		enabled: true,
+		config: {
+			endpoint: process.env.S3_ENDPOINT!,
+			region: process.env.S3_REGION!,
+			credentials: {
+				accessKeyId: process.env.S3_ACCESS_KEY_ID!,
+				secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+			},
+			forcePathStyle: true,
+		},
+	})
 }
