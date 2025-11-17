9fc440bfdf2087d70e0c90042bf4c059d3260a51
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-17 10:40:39 +0100

    add force-dynamic

--- src/app/(frontend)/[locale]/luoghi/page.tsx
+++ src/app/(frontend)/[locale]/luoghi/page.tsx
@@ -2,6 +2,8 @@ import { CollectionGridPage } from '@/modules/components/collection-grid-page'
 
 //
 
+export const dynamic = 'force-dynamic'
+
 export default async function Page() {
 	return <CollectionGridPage collection="luoghi" />
 }
