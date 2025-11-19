d83a60232f256b3772f01daa460cc0dc48e25d46
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2025-11-19 12:44:14 +0100

    feat: articoli

---
+++ src/app/(frontend)/[locale]/articoli/[slug]/page.tsx
@@ -0,0 +1,53 @@
+import { getTranslations } from 'next-intl/server'
+
+import { CollectionPageHeading } from '@/modules/components/collection-page-heading'
+import { Container } from '@/modules/components/container'
+import { Copertina } from '@/modules/components/copertina'
+import { Gallery } from '@/modules/components/gallery'
+import { RichText } from '@/modules/components/richtext'
+import { Badge } from '@/modules/components/shadcn/components/ui/badge'
+import { T } from '@/modules/components/t'
+import { formatDate } from '@/modules/utils'
+import { getRecordBySlug, getSlug, PageWithSlugProps } from '@/modules/utils/server'
+
+//
+
+export default async function ArticoloPage(props: PageWithSlugProps) {
+	const slug = await getSlug(props)
+	const { record: articolo } = await getRecordBySlug('articoli', slug)
+
+	const t = await getTranslations('articoli')
+	const tTags = await getTranslations('tags')
+
+	return (
+		<>
+			<Copertina copertina={articolo.copertina} collection="articoli" />
+			<CollectionPageHeading
+				collection="articoli"
+				title={articolo.name}
+				backButton={{
+					href: '/articoli',
+					children: t('backButton'),
+				}}
+			>
+				<T tag="h3" className="-mt-2 text-white">
+					{articolo.subtitle}
+				</T>
+
+				<div className="flex gap-2 items-center">
+					{articolo.date && (
+						<span className="text-sm font-semibold">{formatDate(articolo.date)}</span>
+					)}
+					{articolo.date && articolo.tag && <span>|</span>}
+					{articolo.tag && <Badge>{tTags(articolo.tag)}</Badge>}
+				</div>
+			</CollectionPageHeading>
+
+			<Container className="max-w-prose">
+				<RichText data={articolo.contents} />
+			</Container>
+
+			<Gallery items={articolo.gallery} collection="articoli" />
+		</>
+	)
+}
